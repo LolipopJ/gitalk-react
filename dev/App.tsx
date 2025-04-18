f3fd6efb1ff7740cf39f9bac0ac931db8e2f648a
LolipopJ <42314340+LolipopJ@users.noreply.github.com>
2025-04-18 13:38:50 +0800

    dev: enable to set theme color and default issue number

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,8 +1,5 @@
-import "../lib/themes/gitalk-light.scss";
-
-// import "../lib/themes/gitalk-dark.scss";
 import { useRequest } from "ahooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ACCESS_TOKEN_KEY } from "../lib/constants";
 import Gitalk from "../lib/gitalk";
@@ -18,23 +15,41 @@ const {
   VITE_ADMIN,
 } = import.meta.env;
 
-const theme = "light";
-
 const PER_PAGE = 30;
 
+type Theme = "light" | "dark";
+
 const App = () => {
-  const [issues, setIssues] = useState<Issue[]>([]);
   const [issuesPage, setIssuesPage] = useState<number>(1);
   const [issuesLoaded, setIssuesLoaded] = useState<boolean>(false);
 
+  const [theme, setTheme] = useState<Theme>("light");
   const [issueNumber, setIssueNumber] = useState<number>();
+
+  useEffect(() => {
+    const url = new URL(location.href);
+    const searchParams = url.searchParams;
+
+    const theme = searchParams.get("theme") || "light";
+    if (theme === "dark") {
+      import("../lib/themes/gitalk-dark.scss");
+      setTheme("dark");
+    } else {
+      import("../lib/themes/gitalk-light.scss");
+      setTheme("light");
+    }
+
+    const initialIssueNumber =
+      Number(searchParams.get("issueNumber")) || undefined;
+    setIssueNumber(initialIssueNumber);
+  }, []);
 
   const octokit = getOctokitInstance(
     localStorage.getItem(ACCESS_TOKEN_KEY) ?? undefined,
   );
 
-  const { loading: getIssuesLoading } = useRequest<void, [number]>(
-    async () => {
+  const { data: issues = [], loading: getIssuesLoading } = useRequest(
+    async (): Promise<Issue[]> => {
       const from = (issuesPage - 1) * PER_PAGE + 1;
       const to = issuesPage * PER_PAGE;
 
@@ -57,21 +72,36 @@ const App = () => {
           setIssuesLoaded(true);
         }
 
-        setIssues((prev) => [...prev, ..._issues]);
+        return [...issues, ..._issues];
       } else {
         logger.e(`Get issues from ${from} to ${to} failed:`, getIssuesRes);
-        throw new Error(JSON.stringify(getIssuesRes));
+        return issues;
       }
     },
     {
-      ready: !!VITE_REPO_OWNER && !!VITE_REPO_NAME,
+      ready: !!VITE_REPO_OWNER && !!VITE_REPO_NAME && !issuesLoaded,
       refreshDeps: [issuesPage],
+      onSuccess: (data) => {
+        if (!issueNumber) {
+          setIssueNumber(data[0]?.number);
+        }
+      },
     },
   );
 
   return (
-    <div>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: 12 }}>
+    <div style={{ padding: 16 }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          gap: 12,
+        }}
+      >
+        <span>
+          Existed issues in repository {VITE_REPO_OWNER}/{VITE_REPO_NAME}:
+        </span>
         {issues.map(({ number }) => (
           <a
             key={number}
@@ -81,6 +111,7 @@ const App = () => {
               border: "1px solid #333",
               borderRadius: "6px",
               cursor: "pointer",
+              backgroundColor: number === issueNumber ? "#efefef" : undefined,
             }}
           >
             {number}
@@ -101,7 +132,7 @@ const App = () => {
         style={{
           marginTop: 16,
           padding: 32,
-          background: theme === "light" ? "#fff" : "#000",
+          background: theme === "light" ? "#eee" : "#111",
         }}
       >
         {!!issueNumber && (
@@ -112,6 +143,8 @@ const App = () => {
             repo={VITE_REPO_NAME}
             admin={JSON.parse(VITE_ADMIN)}
             number={issueNumber}
+            labels={["gitalk-react-dev"]}
+            createIssueManually
           />
         )}
       </div>
